526c637b1ff961ccdef3b164a6fa52e8546a9a56
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-29 02:28:07 +0600

    responsiveness done

--- js/app.js
+++ js/app.js
@@ -10,3 +10,23 @@ $(".slider").slick({
   nextArrow:
     "<button class='btn btn-next' aria-label='Previous' type='button'><img src='./images/next.png' class='img-fluid'/></button>",
 });
+
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: ".slider-nav",
+  arrows: true,
+  prevArrow:
+    "<button class='btn btn-prev' aria-label='Previous' type='button'><img src='./images/back.png' class='img-fluid'/></button>",
+  nextArrow:
+    "<button class='btn btn-next' aria-label='Previous' type='button'><img src='./images/next.png' class='img-fluid'/></button>",
+});
+$(".slider-nav").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+});
